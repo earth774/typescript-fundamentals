3a5d564e26b0c1e070e91239195d88dd84a22b2d
sutthiphong <sutthiphong@sutthiphongs-MacBook-Air.local>
2024-12-08 14:31:49 +0700

    Hands-On : Caches

--- index.ts
+++ index.ts
@@ -1,23 +1,42 @@
-interface CurrencyResult {
+interface CurrencyResult<T extends string> {
     amount: number
     base: string
     date: string
-    rates: {
-        [a: string]: number
-    }
+    rates: Record<T, number>
 }
 
 
 
 class Currency<const Currencies> {
     api = 'https://api.frankfurter.app'
+    private caches: Record<string, number> = {}
     constructor(public currencies: Currencies) {
     }
 
-    convert(from: string, to: string, amount: number) {
-        return fetch(`${this.api}/latest?from=${from}&to=${to}&amount=${amount}`)
-            .then((x) => x.json() as any as CurrencyResult)
+    async convert<To extends Currencies[keyof Currencies]>(
+        from: Currencies[keyof Currencies],
+        to: To,
+        amount: number
+    ) {
+        const key = `${from}${to}${amount}` as To extends string ? To : never;
+
+        if (this.caches[key]) {
+            return this.caches[key]
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 1000))
+
+        const result = await fetch(`${this.api}/latest?from=${from}&to=${to}&amount=${amount}`)
+            .then((x) => x.json() as any as CurrencyResult<
+                To extends string ? To : never
+            >)
             .then((a) => a)
+
+        const rate = result.rates[to as To extends string ? To : never];
+
+        this.caches[key] = rate
+
+        return rate
     }
 
     log(currency: Currencies[keyof Currencies]) {
@@ -32,8 +51,17 @@ class Currency<const Currencies> {
 const myCurrency = new Currency(['USD', 'JPY', 'THB'])
 
 myCurrency.log('USD')
-myCurrency.convert('USD', 'THB', 1).then(console.log)
-myCurrency.latest.then(console.log)
+
+
+const main = async () => {
+    await myCurrency.convert('USD', 'THB', 1).then(console.log)
+    await myCurrency.convert('USD', 'THB', 1).then(console.log)
+    await myCurrency.convert('USD', 'THB', 1).then(console.log)
+    await myCurrency.convert('USD', 'THB', 1).then(console.log)
+}
+
+main()
+// myCurrency.latest.then(console.log)
 
 // const api = 'https://api.frankfurter.app'
 
